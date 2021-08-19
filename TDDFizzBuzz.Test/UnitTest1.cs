46aa787f9940a5de649a39d7941e97d77faa8cfb
negiseijin <satoru6221@gmail.com>
2021-08-19 22:58:39 +0900

    :sparkles: ChainingAssertionの追加

--- TDDFizzBuzz.Test/UnitTest1.cs
+++ TDDFizzBuzz.Test/UnitTest1.cs
@@ -30,60 +30,60 @@ namespace TDDFizzBuzz.Tests
         [InlineData(1, "1")]
         [InlineData(11, "11")]
         [InlineData(23, "23")]
-        public void _3と5の倍数以外の数を文字列に変換する(int num, string value)
+        public void _3と5の倍数以外の数を文字列に変換する(int num, string expected)
         {
             //Act 実行
             var actual = FizzBuzz.GetNum(num);
             //Assert 検証
-            Assert.Equal(value, actual);
+            actual.Is(expected);
         }
 
         [Theory]
         [InlineData(3)]
         [InlineData(6)]
         [InlineData(9)]
-        public void _3の倍数を渡すと文字列Fizzを返す(int value)
+        public void _3の倍数を渡すと文字列Fizzを返す(int num)
         {
             //Act 実行
-            var actual = FizzBuzz.GetNum(value);
+            var actual = FizzBuzz.GetNum(num);
             //Assert 検証
-            Assert.Equal("Fizz", actual);
+            actual.Is("Fizz");
         }
 
         [Theory]
         [InlineData(5)]
         [InlineData(10)]
         [InlineData(20)]
-        public void _5の倍数を渡すと文字列Buzzを返す(int value)
+        public void _5の倍数を渡すと文字列Buzzを返す(int num)
         {
             //Act 実行
-            var actual = FizzBuzz.GetNum(value);
+            var actual = FizzBuzz.GetNum(num);
             //Assert 検証
-            Assert.Equal("Buzz", actual);
+            actual.Is("Buzz");
         }
 
         [Theory]
         [InlineData(15)]
         [InlineData(30)]
         [InlineData(60)]
-        public void _3と5の倍数を渡すと文字列FizzBuzzを返す(int value)
+        public void _3と5の倍数を渡すと文字列FizzBuzzを返す(int num)
         {
             //Act 実行
-            var actual = FizzBuzz.GetNum(value);
+            var actual = FizzBuzz.GetNum(num);
             //Assert 検証
-            Assert.Equal("FizzBuzz", actual);
+            actual.Is("FizzBuzz");
         }
 
         [Theory]
         [InlineData(0)]
         [InlineData(101)]
         [InlineData(-1)]
-        public void _1から100以外の数を渡すと例外を返す(int value)
+        public void _1から100以外の数を渡すと例外を返す(int num)
         {
             //Act 実行
-            var actual = Assert.Throws<ArgumentException>(() => FizzBuzz.GetNum(value));
+            var actual = Assert.Throws<ArgumentException>(() => FizzBuzz.GetNum(num));
             //Assert 検証
-            Assert.Equal("1から100までの数字を入力してください", actual.Message);
+            actual.Message.Is("1から100までの数字を入力してください");
         }
 
     }
